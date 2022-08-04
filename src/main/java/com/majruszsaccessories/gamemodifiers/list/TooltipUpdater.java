c4e8271e403bd51ce90aede2fb04a54cc6f07be4
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-08-04 20:01:15 +0200

    Added base elements

---
+++ src/main/java/com/majruszsaccessories/gamemodifiers/list/TooltipUpdater.java
@@ -0,0 +1,9 @@
+package com.majruszsaccessories.gamemodifiers.list;
+
+import com.majruszsaccessories.gamemodifiers.AccessoryModifier;
+
+public class TooltipUpdater extends AccessoryModifier {
+	public TooltipUpdater() {
+		super( AccessoryModifier.DEFAULT, "TooltipUpdater", "" );
+	}
+}
--- src/main/java/com/majruszsaccessories/items/AccessoryItem.java
+++ src/main/java/com/majruszsaccessories/items/AccessoryItem.java
@@ -2,10 +2,29 @@ package com.majruszsaccessories.items;
 
 import com.majruszsaccessories.Registries;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 
 public class AccessoryItem extends Item {
 	public AccessoryItem( String configName, String registryKey ) {
 		super( new Properties().stacksTo( 1 ).rarity( Rarity.RARE ).tab( Registries.ITEM_GROUP ) );
+	}
+
+	public static double getBonus( ItemStack itemStack ) {
+		return itemStack.getItem() instanceof AccessoryItem ? itemStack.getOrCreateTagElement( Tags.BONUS ).getDouble( Tags.VALUE ) : 0.0;
+	}
+
+	public static boolean hasBonusTag( ItemStack itemStack ) {
+		return itemStack.getTagElement( Tags.BONUS ) != null;
+	}
+
+	static final class Tooltips {
+		static final String INVENTORY = "majruszsaccessories.items.accessory_item";
+		static final String BONUS = "majruszsaccessories.items.bonus";
+	}
+
+	static final class Tags {
+		static final String BONUS = "Bonus";
+		static final String VALUE = "Value";
 	}
 }
