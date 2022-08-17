654882ff723f5c277ede8704de95240a46a953a1
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-08-17 20:48:19 +0200

    Added possibility to get Angler Emblem when difficulty mod is not installed

--- src/main/java/com/majruszsaccessories/items/AnglerEmblemItem.java
+++ src/main/java/com/majruszsaccessories/items/AnglerEmblemItem.java
@@ -1,17 +1,20 @@
 package com.majruszsaccessories.items;
 
+import com.majruszsaccessories.AccessoryHandler;
 import com.majruszsaccessories.Integration;
 import com.majruszsaccessories.Registries;
 import com.majruszsaccessories.gamemodifiers.list.FishingLuckBonus;
 import com.majruszsdifficulty.items.TreasureBagItem;
-import com.mlib.MajruszLibrary;
 import com.mlib.config.ConfigGroup;
+import com.mlib.gamemodifiers.Condition;
 import com.mlib.gamemodifiers.GameModifier;
 import com.mlib.gamemodifiers.GameModifiersHolder;
+import com.mlib.gamemodifiers.contexts.OnItemFishedContext;
 import com.mlib.gamemodifiers.contexts.OnLootTableCustomLoadContext;
-import com.mlib.gamemodifiers.contexts.OnLootTableLoadContext;
+import com.mlib.gamemodifiers.data.OnItemFishedData;
 import com.mlib.gamemodifiers.data.OnLootTableCustomLoadData;
-import com.mlib.gamemodifiers.data.OnLootTableLoadData;
+import com.mlib.levels.LevelHelper;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 
 import java.util.function.Supplier;
@@ -25,28 +28,40 @@ public class AnglerEmblemItem extends AccessoryItem {
 	public static Supplier< AnglerEmblemItem > create() {
 		GameModifiersHolder< AnglerEmblemItem > holder = new GameModifiersHolder<>( ID, AnglerEmblemItem::new );
 		holder.addModifier( FishingLuckBonus::new );
-		holder.addModifier( AddToTreasureBag::new );
+		holder.addModifier( AddDropChance::new );
 
 		return holder::getRegistry;
 	}
 
-	static class AddToTreasureBag extends GameModifier {
+	static class AddDropChance extends GameModifier {
 		final Supplier< ? extends AccessoryItem > item;
 
-		public AddToTreasureBag( Supplier< ? extends AccessoryItem > item, String configKey ) {
+		public AddDropChance( Supplier< ? extends AccessoryItem > item, String configKey ) {
 			super( configKey, "", "" );
 			this.item = item;
 
-			OnLootTableCustomLoadContext onLoad = new OnLootTableCustomLoadContext( this::addLoot );
-			onLoad.addCondition( data->Integration.isProgressiveDifficultyInstalled() )
-				.addCondition( data->TreasureBagItem.Fishing.LOCATION.equals( data.name ) );
+			if( Integration.isProgressiveDifficultyInstalled() ) {
+				OnLootTableCustomLoadContext onLoad = new OnLootTableCustomLoadContext( this::addToTreasureBag );
+				onLoad.addCondition( data->TreasureBagItem.Fishing.LOCATION.equals( data.name ) );
 
-			this.addContext( onLoad );
+				this.addContext( onLoad );
+			} else {
+				OnItemFishedContext onFished = new OnItemFishedContext( this::onFished );
+				onFished.addCondition( new Condition.Chance( 0.00375, "drop_chance", "Chance to drop Angler Emblem from fishing." ) );
+
+				this.addContext( onFished );
+			}
 		}
 
-		private void addLoot( OnLootTableCustomLoadData data ) {
+		private void addToTreasureBag( OnLootTableCustomLoadData data ) {
 			int poolId = data.addPool();
 			data.addEntry( poolId, this.item.get(), 1, 4, LootItemRandomChanceCondition.randomChance( 0.075f ) );
+		}
+
+		private void onFished( OnItemFishedData data ) {
+			assert data.level != null;
+			ItemStack anglerEmblem = AccessoryHandler.construct( this.item.get() );
+			LevelHelper.spawnItemEntityFlyingTowardsDirection( anglerEmblem, data.level, data.hook.position(), data.player.position() );
 		}
 	}
 }
