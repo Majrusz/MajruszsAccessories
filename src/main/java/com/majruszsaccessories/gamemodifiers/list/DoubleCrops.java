f72394bcfa0359d45ca60706542f9e0c9193a062
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-08-27 18:00:40 +0200

    Code refactored

--- src/main/java/com/majruszsaccessories/gamemodifiers/list/DoubleCrops.java
+++ src/main/java/com/majruszsaccessories/gamemodifiers/list/DoubleCrops.java
@@ -7,8 +7,7 @@ import com.majruszsaccessories.items.AccessoryItem;
 import com.mlib.blocks.BlockHelper;
 import com.mlib.effects.ParticleHandler;
 import com.mlib.gamemodifiers.Condition;
-import com.mlib.gamemodifiers.contexts.OnLootContext;
-import com.mlib.gamemodifiers.data.OnLootData;
+import com.mlib.gamemodifiers.contexts.OnLoot;
 import net.minecraft.world.entity.LivingEntity;
 
 import java.util.ArrayList;
@@ -16,13 +15,13 @@ import java.util.function.Supplier;
 
 public class DoubleCrops extends AccessoryModifier {
 	final AccessoryPercent chance = new AccessoryPercent( "double_crops_chance", "Chance to double crops when harvesting.", false, 0.25, 0.0, 1.0 );
-  	
+
 	public DoubleCrops( Supplier< ? extends AccessoryItem > item, String configKey ) {
 		super( item, configKey, "", "" );
 
-		OnLootContext onLoot = new OnLootContext( this.toAccessoryConsumer( this::doubleLoot, this.chance ) );
+		OnLoot.Context onLoot = new OnLoot.Context( this.toAccessoryConsumer( this::doubleLoot, this.chance ) );
 		onLoot.addCondition( new Condition.IsServer() )
-			.addCondition( OnLootContext.HAS_ORIGIN )
+			.addCondition( OnLoot.HAS_ORIGIN )
 			.addCondition( data->data.blockState != null && BlockHelper.isCropAtMaxAge( data.blockState ) )
 			.addCondition( data->data.entity instanceof LivingEntity )
 			.addConfig( this.chance );
@@ -31,7 +30,7 @@ public class DoubleCrops extends AccessoryModifier {
 		this.addTooltip( this.chance, "majruszsaccessories.bonuses.double_crops" );
 	}
 
-	private void doubleLoot( OnLootData data, AccessoryHandler handler ) {
+	private void doubleLoot( OnLoot.Data data, AccessoryHandler handler ) {
 		ParticleHandler.AWARD.spawn( data.level, data.origin, 6 );
 		data.generatedLoot.addAll( new ArrayList<>( data.generatedLoot ) );
 	}
