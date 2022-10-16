ff39030520dfd77b5e4d9620bcaf3d6c77207154
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-10-16 21:02:08 +0200

    Added possibility to swing the flag

--- src/main/java/com/majruszsaccessories/items/WhiteFlagItem.java
+++ src/main/java/com/majruszsaccessories/items/WhiteFlagItem.java
@@ -1,9 +1,12 @@
 package com.majruszsaccessories.items;
 
 import com.majruszsaccessories.Registries;
+import com.majruszsaccessories.gamemodifiers.AccessoryModifier;
+import com.mlib.Utility;
 import com.mlib.config.ConfigGroup;
 import com.mlib.gamemodifiers.GameModifier;
 import com.mlib.gamemodifiers.GameModifiersHolder;
+import com.mlib.gamemodifiers.contexts.OnPlayerInteract;
 
 import java.util.function.Supplier;
 
@@ -15,7 +18,25 @@ public class WhiteFlagItem extends AccessoryItem {
 
 	public static Supplier< WhiteFlagItem > create() {
 		GameModifiersHolder< WhiteFlagItem > holder = AccessoryItem.newHolder( ID, WhiteFlagItem::new );
+		holder.addModifier( SwingBehavior::new );
 
 		return holder::getRegistry;
+	}
+
+	static class SwingBehavior extends AccessoryModifier {
+		public SwingBehavior( Supplier< ? extends AccessoryItem > item, String configKey ) {
+			super( item, configKey, "", "" );
+
+			OnPlayerInteract.Context onPlayerInteract = new OnPlayerInteract.Context( this::swing );
+			onPlayerInteract.addCondition( data->data.itemStack.getItem() instanceof WhiteFlagItem )
+				.addCondition( data->!data.player.getCooldowns().isOnCooldown( data.itemStack.getItem() ) );
+
+			this.addContext( onPlayerInteract );
+		}
+
+		private void swing( OnPlayerInteract.Data data ) {
+			data.player.swing( data.hand );
+			data.player.getCooldowns().addCooldown( data.itemStack.getItem(), Utility.secondsToTicks( 0.5 ) );
+		}
 	}
 }
