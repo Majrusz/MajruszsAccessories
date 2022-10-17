6176e60cfd288c75d00ceb6a6623eae77b6828c6
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-10-17 17:59:28 +0200

    Added White Flag to village chests

--- src/main/java/com/majruszsaccessories/items/WhiteFlagItem.java
+++ src/main/java/com/majruszsaccessories/items/WhiteFlagItem.java
@@ -6,11 +6,16 @@ import com.majruszsaccessories.gamemodifiers.list.BaseOffer;
 import com.majruszsaccessories.gamemodifiers.list.ReduceDamageDealt;
 import com.majruszsaccessories.gamemodifiers.list.ReduceDamageReceived;
 import com.mlib.Utility;
+import com.mlib.blocks.BlockHelper;
 import com.mlib.config.ConfigGroup;
+import com.mlib.gamemodifiers.Condition;
 import com.mlib.gamemodifiers.GameModifier;
 import com.mlib.gamemodifiers.GameModifiersHolder;
+import com.mlib.gamemodifiers.contexts.OnLoot;
 import com.mlib.gamemodifiers.contexts.OnPlayerInteract;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 
 import java.util.function.Supplier;
 
@@ -25,6 +30,7 @@ public class WhiteFlagItem extends AccessoryItem {
 		holder.addModifier( ReduceDamageReceived::new );
 		holder.addModifier( ReduceDamageDealt::new );
 		holder.addModifier( SwingBehavior::new );
+		holder.addModifier( AddToVillageChests::new );
 		holder.addModifier( TradeOffer::new );
 
 		return holder::getRegistry;
@@ -44,6 +50,22 @@ public class WhiteFlagItem extends AccessoryItem {
 		private void swing( OnPlayerInteract.Data data ) {
 			data.player.swing( data.hand );
 			data.player.getCooldowns().addCooldown( data.itemStack.getItem(), Utility.secondsToTicks( 0.5 ) );
+		}
+	}
+
+	static class AddToVillageChests extends AccessoryModifier {
+		public AddToVillageChests( Supplier< ? extends AccessoryItem > item, String configKey ) {
+			super( item, configKey, "", "" );
+
+			OnLoot.Context onLoot = new OnLoot.Context( this::addToGeneratedLoot );
+			onLoot.addCondition( new Condition.IsServer() )
+				.addCondition( new Condition.Chance( 0.15, "spawn_chance", "Chance for White Flag to spawn in any village chest." ) )
+				.addCondition( OnLoot.HAS_ORIGIN )
+				.addCondition( data->BlockHelper.getBlockEntity( data.level, data.origin ) instanceof RandomizableContainerBlockEntity )
+				.addCondition( data->data.entity instanceof ServerPlayer )
+				.addCondition( data->data.context.getQueriedLootTableId().toString().contains( "minecraft:chests/village" ) );
+
+			this.addContext( onLoot );
 		}
 	}
 
