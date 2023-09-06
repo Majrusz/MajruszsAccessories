e8127bc9c15a40039702701142b0eb83b52cab44
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-09-06 08:44:21 +0200

    Optimized Peace Emblem bonus

--- src/main/java/com/majruszsaccessories/boosters/components/LowerSpawnRate.java
+++ src/main/java/com/majruszsaccessories/boosters/components/LowerSpawnRate.java
@@ -7,16 +7,19 @@ import com.mlib.config.ConfigGroup;
 import com.mlib.config.DoubleConfig;
 import com.mlib.contexts.OnMobSpawnLimit;
 import com.mlib.contexts.OnMobSpawnRate;
+import com.mlib.contexts.OnServerTick;
 import com.mlib.contexts.base.Condition;
 import com.mlib.math.Range;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.server.ServerLifecycleHooks;
 
 import java.util.function.Supplier;
 
 public class LowerSpawnRate extends BoosterComponent {
 	final DoubleConfig spawnRateReduction;
+	float currentMultiplier = 1.0f;
 
 	public static ISupplier create( double chance ) {
 		return ( item, group )->new LowerSpawnRate( item, group, chance );
@@ -26,34 +29,39 @@ public class LowerSpawnRate extends BoosterComponent {
 		super( item );
 
 		this.spawnRateReduction = new DoubleConfig( spawnRateReduction, new Range<>( 0.01, 0.99 ) );
-		group.addConfig( this.spawnRateReduction.name( "spawn_rate_reduction" ).comment( "Decreases spawn rate of all hostile mobs." ) );
+		group.addConfig( this.spawnRateReduction.name( "spawn_rate_reduction" ).comment( "Decreases spawn rate of all hostile mobs for all dimensions." ) );
 
 		OnMobSpawnRate.listen( this::decreaseSpawnRate )
 			.addCondition( OnMobSpawnRate.is( MobCategory.MONSTER ) )
-			.addCondition( hasAnyone( item ) )
 			.insertTo( group );
 
 		OnMobSpawnLimit.listen( this::decreaseSpawnLimit )
 			.addCondition( OnMobSpawnLimit.is( MobCategory.MONSTER ) )
-			.addCondition( hasAnyone( item ) )
+			.insertTo( group );
+
+		OnServerTick.listen( this::updateSpawnMultiplier )
+			.addCondition( Condition.cooldown( 1.0, Dist.DEDICATED_SERVER ) )
 			.insertTo( group );
 
 		this.addTooltip( "majruszsaccessories.boosters.lower_spawn_rate", TooltipHelper.asItem( item ), TooltipHelper.asFixedPercent( this.spawnRateReduction ) );
 	}
 
 	private void decreaseSpawnRate( OnMobSpawnRate.Data data ) {
-		data.value *= 1.0f - this.spawnRateReduction.get();
+		data.value *= this.currentMultiplier;
 	}
 
 	private void decreaseSpawnLimit( OnMobSpawnLimit.Data data ) {
-		data.value *= 1.0f - this.spawnRateReduction.get();
+		data.value *= this.currentMultiplier;
 	}
 
-	private < Type > Condition< Type > hasAnyone( Supplier< BoosterItem > item ) {
-		return Condition.predicate( data->{
-			MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+	private void updateSpawnMultiplier( OnServerTick.Data data ) {
+		MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+		if( server != null ) {
+			long boostersCount = server.getPlayerList().getPlayers().stream().filter( player->AccessoryHolder.hasBooster( player, this.item.get() ) ).count();
 
-			return server != null && server.getPlayerList().getPlayers().stream().anyMatch( player->AccessoryHolder.hasBooster( player, item.get() ) );
-		} );
+			this.currentMultiplier = ( float )Math.pow( 1.0f - this.spawnRateReduction.get(), boostersCount );
+		} else {
+			this.currentMultiplier = 1.0f;
+		}
 	}
 }
