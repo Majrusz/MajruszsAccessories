9c85e90b0dec85648eb82ca61cfd16d46ce31921
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-09-08 23:56:54 +0200

    Fixed bug with Golden Horseshoe not applying luck bonus properly

--- src/main/java/com/majruszsaccessories/boosters/components/LuckBonus.java
+++ src/main/java/com/majruszsaccessories/boosters/components/LuckBonus.java
@@ -6,9 +6,10 @@ import com.majruszsaccessories.tooltip.TooltipHelper;
 import com.mlib.attributes.AttributeHandler;
 import com.mlib.config.ConfigGroup;
 import com.mlib.config.IntegerConfig;
-import com.mlib.contexts.base.Condition;
 import com.mlib.contexts.OnPlayerTick;
+import com.mlib.contexts.base.Condition;
 import com.mlib.math.Range;
+import com.mlib.text.TextHelper;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraftforge.api.distmarker.Dist;
@@ -16,7 +17,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import java.util.function.Supplier;
 
 public class LuckBonus extends BoosterComponent {
-	static final AttributeHandler LUCK_ATTRIBUTE = new AttributeHandler( "ad04ffd5-fb72-4e53-83e7-a388bd7e1e1f", "HorseshoeLuckBonus", Attributes.LUCK, AttributeModifier.Operation.ADDITION );
+	final AttributeHandler attribute;
 	final IntegerConfig luckBonus;
 
 	public static ISupplier create( int luck ) {
@@ -26,6 +27,12 @@ public class LuckBonus extends BoosterComponent {
 	protected LuckBonus( Supplier< BoosterItem > item, ConfigGroup group, int luck ) {
 		super( item );
 
+		this.attribute = new AttributeHandler(
+			TextHelper.createUUIDFromBytes( group.getName() ).toString(),
+			String.format( "%sLuckBonus", group.getName() ),
+			Attributes.LUCK,
+			AttributeModifier.Operation.ADDITION
+		);
 		this.luckBonus = new IntegerConfig( luck, new Range<>( 1, 10 ) );
 
 		OnPlayerTick.listen( this::updateLuck )
@@ -40,6 +47,6 @@ public class LuckBonus extends BoosterComponent {
 		AccessoryHolder holder = AccessoryHolder.find( data.player, this.item.get() );
 		int luck = holder.isValid() ? holder.apply( this.luckBonus ) : 0;
 
-		LUCK_ATTRIBUTE.setValue( luck ).apply( data.player );
+		attribute.setValue( luck ).apply( data.player );
 	}
 }
