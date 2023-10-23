2e82368b8a664720e86d25f2f10c497f6cc47b63
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-10-23 23:27:04 +0200

    Added particles to Swimmer Guide

--- common/src/main/java/com/majruszsaccessories/accessories/components/SwimmingSpeedBonus.java
+++ common/src/main/java/com/majruszsaccessories/accessories/components/SwimmingSpeedBonus.java
@@ -8,7 +8,10 @@ import com.majruszsaccessories.items.AccessoryItem;
 import com.majruszsaccessories.tooltip.TooltipHelper;
 import com.mlib.contexts.OnEntitySwimSpeedMultiplierGet;
 import com.mlib.data.Serializable;
+import com.mlib.math.AnyPos;
 import com.mlib.math.Range;
+import com.mlib.time.TimeHelper;
+import net.minecraft.server.level.ServerLevel;
 
 public class SwimmingSpeedBonus extends BonusComponent< AccessoryItem > {
 	RangedFloat multiplier = new RangedFloat().id( "multiplier" ).maxRange( Range.of( 0.0f, 10.0f ) );
@@ -33,5 +36,15 @@ public class SwimmingSpeedBonus extends BonusComponent< AccessoryItem > {
 
 	private void increaseSwimSpeed( OnEntitySwimSpeedMultiplierGet data ) {
 		data.multiplier *= 1.0f + CustomConditions.getLastHolder().apply( this.multiplier );
+		if( data.entity.isInWater() && data.getLevel() instanceof ServerLevel && TimeHelper.haveTicksPassed( 5 ) ) {
+			this.spawnEffects( data );
+		}
+	}
+
+	private void spawnEffects( OnEntitySwimSpeedMultiplierGet data ) {
+		CustomConditions.getLastHolder()
+			.getParticleEmitter()
+			.count( 1 )
+			.emit( data.getServerLevel(), AnyPos.from( data.entity.position() ).add( 0.0f, 0.5f * data.entity.getBbHeight(), 0.0f ).vec3() );
 	}
 }
