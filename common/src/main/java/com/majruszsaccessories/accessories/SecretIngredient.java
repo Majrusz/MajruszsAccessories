20db79886704171b571ac8e0b97fcaf1b606ac16
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-10-23 22:13:58 +0200

    Added Secret Ingredient when brewing potions

--- common/src/main/java/com/majruszsaccessories/accessories/SecretIngredient.java
+++ common/src/main/java/com/majruszsaccessories/accessories/SecretIngredient.java
@@ -4,7 +4,16 @@ import com.majruszsaccessories.MajruszsAccessories;
 import com.majruszsaccessories.accessories.components.StrongerPotions;
 import com.majruszsaccessories.accessories.components.TradeOffer;
 import com.majruszsaccessories.common.AccessoryHandler;
+import com.majruszsaccessories.common.BonusComponent;
+import com.majruszsaccessories.common.BonusHandler;
+import com.majruszsaccessories.contexts.base.CustomConditions;
+import com.majruszsaccessories.items.AccessoryItem;
 import com.mlib.annotation.AutoInstance;
+import com.mlib.contexts.OnItemBrewed;
+import com.mlib.data.Serializable;
+import com.mlib.level.LevelHelper;
+import com.mlib.math.AnyPos;
+import com.mlib.math.Range;
 import net.minecraft.world.entity.npc.VillagerProfession;
 
 @AutoInstance
@@ -13,6 +22,31 @@ public class SecretIngredient extends AccessoryHandler {
 		super( MajruszsAccessories.SECRET_INGREDIENT );
 
 		this.add( StrongerPotions.create( 0.6f, 1 ) )
+			.add( BrewingDropChance.create() )
 			.add( TradeOffer.create( VillagerProfession.CLERIC, 5 ) );
+	}
+
+	static class BrewingDropChance extends BonusComponent< AccessoryItem > {
+		float chance = 0.0334f;
+
+		public static ISupplier< AccessoryItem > create() {
+			return BrewingDropChance::new;
+		}
+
+		protected BrewingDropChance( BonusHandler< AccessoryItem > handler ) {
+			super( handler );
+
+			OnItemBrewed.listen( this::spawnAccessory )
+				.addCondition( CustomConditions.dropChance( ()->this.chance, data->LevelHelper.getNearestPlayer( data.getLevel(), data.blockPos, 30.0f ) ) );
+
+			Serializable config = handler.getConfig();
+			config.defineFloat( "brewing_drop_chance", ()->this.chance, x->this.chance = Range.CHANCE.clamp( x ) );
+		}
+
+		private void spawnAccessory( OnItemBrewed data ) {
+			AnyPos pos = AnyPos.from( data.blockPos ).center();
+
+			this.spawnFlyingItem( data.getLevel(), pos.vec3(), pos.add( 0.0f, 1.0f, 0.0f ).vec3() );
+		}
 	}
 }
